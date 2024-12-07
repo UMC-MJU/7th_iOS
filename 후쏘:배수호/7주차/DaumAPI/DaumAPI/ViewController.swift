0a108a48c52ae29d5e5a631fe65a3de154350d1c
bae-suho <sugh6019@naver.com>
2024-12-07 16:32:02 +0900

    7주차 Daum API 연습

---
+++ 후쏘:배수호/7주차/DaumAPI/DaumAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaumAPI
+//
+//  Created by 배수호 on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+    }
+
+
+}
+
